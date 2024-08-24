7f069b342ca083afabf4d123e1243f4ed6c1ea46
Laurence Ramirez <rhenz8823@gmail.com>
2024-08-24 11:23:48 +0800

    please gumana kana

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,6 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: "public", // Ensure Vite outputs to 'dist'
+        outDir: "public", // Ensure Vite outputs to 'public'
     },
 });
